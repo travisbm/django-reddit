e4a80a8bfa8a9b2875ae3ca6996f0c99ea39b494
Travis Montgomery <travisbm@gmail.com>
2016-08-15 19:10:10 -0500

    commit before migrating post model

--- posts/models.py
+++ posts/models.py
@@ -2,4 +2,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    post_text  = models.CharField(max_length=500)
+    pub_date   = models.DateTimeField('date published')
+    up_votes   = models.IntegerField(default=0)
+    down_votes = models.IntegerField(default=0)
+
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'posts/index.html'
+
+    def get_queryset(self):
+        return Post.objecst.all()
+
+# def index(request):
+#     return render(request, 'posts/index.html')
